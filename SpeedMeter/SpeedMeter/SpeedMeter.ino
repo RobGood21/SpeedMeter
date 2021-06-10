cc4d7c84a62f41ce5a887e7fbc5a1aa9d861847f
Rob Antonisse <robantonisse@gmail.com>
2021-06-10 11:51:42 +0200

    Start SpeedMeter

---
+++ SpeedMeter/SpeedMeter/SpeedMeter.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		SpeedMeter.ino
+ Created:	6/10/2021 11:49:37 AM
+ Author:	RobAntonisse
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
